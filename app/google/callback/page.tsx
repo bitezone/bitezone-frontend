33786fcd97354a085bd105a164cc4f8a7cd568ce
Soney <sonephyo7777777@gmail.com>
2025-04-13 19:58:47 -0400

    abandon: simpleJWT will be implemented instead

--- app/google/callback/page.tsx
+++ app/google/callback/page.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
+import axios from "axios";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
 
-const page = () => {
-  return (
-    <div>Loading</div>
-  )
-}
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-export default page
+  useEffect(() => {
+    const code = searchParams.get("code");
+    const error = searchParams.get("error");
+    if (error) {
+      console.error(error);
+    }
+    if (code) {
+      try {
+        axios
+          .post(
+            process.env.BACKEND_URL + "/api/v1/auth/google/exchange-code/",
+            { code }
+          )
+          .then((res) => {
+            console.log(res.data.access_token);
+            localStorage.setItem("accessToken", res.data.access_token);
+            localStorage.setItem("idToken", res.data.id_token);
+            localStorage.setItem("refreshToken", res.data.refresh_token);
+          })
+          .then(() => {
+            console.log({
+              access_token: localStorage.getItem("accessToken"),
+              code: code,
+              id_token: { id_token: localStorage.getItem("idToken") },
+            });
+            axios.post(process.env.BACKEND_URL + "/api/v1/auth/google/", {
+              access_token: localStorage.getItem("accessToken"),
+              code: code,
+              id_token: { id_token: localStorage.getItem("idToken") },
+            });
+          });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }, [router, searchParams]);
+
+  return <div>Loading</div>;
+};
+
+export default Page;
---
+++ lib/loader.ts
@@ -0,0 +1,33 @@
+import "./index.css";
+import { redirect } from "next/navigation";
+export const handleGoogleCallback = async ({ request }) => {
+  // Exchange callback's code for JWT tokens
+
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+
+  if (code) {
+    try {
+      const response = await fetch(
+        "http://localhost:8000/api/v1/auth/google/",
+        {
+          method: "POST",
+          body: JSON.stringify({ code }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const jwtData = await response.json();
+      console.log(jwtData);
+
+      return redirect("/");
+    } catch (err) {
+      console.error(err);
+      throw new Response("Bad request", { status: 400 });
+    }
+  }
+
+  throw new Response("Not Found", { status: 404 });
+};
