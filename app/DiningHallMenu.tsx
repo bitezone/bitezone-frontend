df143fc60024da49065bb87881cab42bc3281bee
Soney <sonephyo7777777@gmail.com>
2025-04-13 15:45:17 -0400

    wip: setting up frontend for sign in

--- app/DiningHallMenu.tsx
+++ app/DiningHallMenu.tsx
@@ -9,9 +9,13 @@ const DiningHallMenu = () => {
   const [menuDate, setMenuDate] = useState<Date>(new Date());
   const [menuTime, setMenuTime] = useState<string>("");
   const [menuLocation, setMenuLocation] = useState<string>("lakeside");
+  const googleSignInUrl = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&prompt=consent&response_type=code&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&scope=openid%20email%20profile&access_type=offline`;
 
   return (
     <div className="min-h-screen bg-green-50 p-6 flex flex-col items-center gap-5">
+      <div style={{ padding: 20 }}>
+        <a href={googleSignInUrl}>Sign in with Google</a>
+      </div>
       <MenuNavigation
         setMenuDate={setMenuDate}
         setMenuTime={setMenuTime}
